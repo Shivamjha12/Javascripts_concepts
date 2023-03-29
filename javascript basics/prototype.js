4b60ee7c1b3ca02b04d7e3f4f388f8e01b06730c
Shivamjha12 <Jhasapna544@gmail.com>
2023-03-29 16:36:25 +0530

    29/03/2023 04:36 pm

--- javascript basics/prototype.js
+++ javascript basics/prototype.js
@@ -5,3 +5,21 @@ const user = {
     rollno:2001330130151
 };
 console.log(user.name);
+
+const obj = {
+    className:"obj class",
+    __proto__: user
+}
+
+console.log(obj.name + " - " + obj.className);
+
+const userlist = ["shivam jha","Satyam","harendra"];
+
+Array.prototype.get_list_dic=function(){
+    dic={};
+    this.forEach(element => {
+        dic[element]=element;
+    });
+    return dic;
+}
+console.log(userlist.get_list_dic());
